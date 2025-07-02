042f9be403eb811e18b9389c7ba91d65741a9f4c
satyabrat <satyabrat34@gmail.com>
2025-07-02 08:20:55 +0530

    crud operation

--- prisma-js/controllers/user.controller.js
+++ prisma-js/controllers/user.controller.js
@@ -6,30 +6,91 @@ export const createUser = async (req, res) => {
   try {
     // Validate required fields
     if (!name || !email || !password) {
-      res.status(400).json({ message: "All fields are required" });
-      return;
+      return res.status(400).json({ message: "All fields are required" });
     }
 
     // Validate email format
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
-      res.status(400).json({ message: "Invalid email format" });
-      return;
+      return res.status(400).json({ message: "Invalid email format" });
     }
     // Check if user already exists
     const existingUser = await prisma.user.findUnique({ where: { email } });
     if (existingUser) {
-      res.status(400).json({ message: "User already exists" });
-      return;
+      return res.status(400).json({ message: "User already exists" });
     }
 
     const newUser = await prisma.user.create({
       data: { name, email, password },
     });
 
-    res.status(201).json({ message: "User created successfully", newUser });
+    return res
+      .status(201)
+      .json({ message: "User created successfully", newUser });
   } catch (error) {
-    res
+    return res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+};
+
+//update user
+export const updateUser = async (req, res) => {
+  const { id } = req.params;
+  const { name, email, password } = req.body;
+
+  try {
+    const updatedUser = await prisma.user.update({
+      where: { id: Number(id) },
+      data: { name, email, password },
+    });
+
+    return res
+      .status(200)
+      .json({ message: "User updated successfully", updatedUser });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+};
+
+//get all users
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    return res
+      .status(200)
+      .json({ message: "Users fetched successfully", users });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+};
+
+//get user by id
+export const getUserById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await prisma.user.findUnique({ where: { id: Number(id) } });
+    return res.status(200).json({ message: "User fetched successfully", user });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+};
+
+
+//delete user
+export const deleteUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await prisma.user.delete({ where: { id: Number(id) } });
+    return res.status(200).json({ message: "User deleted successfully", user });
+  } catch (error) {
+    return res
       .status(500)
       .json({ message: "Internal server error", error: error.message });
   }
--- prisma-js/routes/userRoute.js
+++ prisma-js/routes/userRoute.js
@@ -1,9 +1,18 @@
 import express from "express";
-import { createUser } from "../controllers/user.controller.js";
+import {
+  createUser,
+  updateUser,
+  getAllUsers,
+  getUserById,
+  deleteUser,
+} from "../controllers/user.controller.js";
 import { Router } from "express";
 
 const router = Router();
 
 router.post("/create", createUser);
-
+router.put("/update/:id", updateUser);
+router.get("/all", getAllUsers);
+router.get("/:id", getUserById);
+router.delete("/delete/:id", deleteUser);
 export default router;
